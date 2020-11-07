fb9fc901c5722fa2a7acc38c759f6c2f2a89e1f3
takaki Ikebe <takakiikebe@me.com>
2020-11-08 01:03:11 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Film-List</title>
+        <title>Directors-List.com</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Contents-List
+                    Directors-List
                 </div>
 
                 <div class="links">
