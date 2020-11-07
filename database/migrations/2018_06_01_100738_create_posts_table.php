1be37aa3225a340b6193fd6478d62778e94fa66e
takaki Ikebe <takakiikebe@me.com>
2020-11-07 18:46:05 +0900

    Initial commit

---
+++ database/migrations/2018_06_01_100738_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title')->nullable();
+            $table->integer('release')->nullable();
+            $table->text('content')->nullable();
+            $table->string('genre')->nullable();
+            $table->string('url1')->nullable();
+            $table->string('url2')->nullable();
+            $table->integer('category_id')->nullable();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,76 @@
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">
+            Sidebar
+        </div>
+
+        <div class="card-body">
+            <ul class="nav" role="tablist">
+                <li role="presentation">
+                [ Listing ]
+                    <a href="http://www.artist-link.com/admin/posts?search=洋楽">
+                        洋楽
+                    </a>
+                    <a href="http://www.artist-link.com/admin/posts?search=インスト">
+                        ジャズ等
+                    </a>
+                    <a href="http://www.artist-link.com/admin/posts?search=日本">
+                        日本
+                    </a>
+                </li>
+                   ------------------------
+                <li role="presentation">
+                    [ Genre ]
+                    <a href="http://artist-link.com/admin/posts?search=hard rock">
+                        Hard_Rock
+                    </a>            
+                    <a href="http://artist-link.com/admin/posts?search=soft rock">
+                        Soft_Rock
+                    </a>            
+                    <a href="http://artist-link.com/admin/posts?search=pops">
+                        Pops
+                    </a>            
+                    <a href="http://artist-link.com/admin/posts?search=rb">
+                        R&B
+                    </a>            
+                    <a href="http://artist-link.com/admin/posts?search=danceable">
+                        Danceable
+                    </a>            
+                    <a href="http://artist-link.com/admin/posts?search=jazz">
+                        Jazz
+                    </a>            
+                    <a href="http://artist-link.com/admin/posts?search=new age">
+                        New_Age
+                    </a> 
+                    <a href="http://artist-link.com/admin/posts?search=kayokyoku">
+                        KayoKyoku
+                    </a>            
+                    <a href="http://artist-link.com/admin/posts?search=folk">
+                        Folk
+                    </a>
+                    <a href="http://artist-link.com/admin/posts?search=new music">
+                        New_Music
+                    </a>            
+                    <a href="http://artist-link.com/admin/posts?search=jpop">
+                        JPOP
+                    </a> 
+                    <a href="http://artist-link.com/admin/posts?search=music">
+                        Other Music
+                    </a>            
+                </li>
+                   ------------------------
+            </ul>
+            <ul class="nav" role="tablist">         
+                <li role="presentation">
+                    <a href="http://mylist24.com/">
+                        All Site
+                    </a>
+                        //     
+                     <a href="http://album-list.com/">
+                        Album
+                    </a>                        
+                </li>
+            </ul>         
+        </div>
+    </div>
+</div>
